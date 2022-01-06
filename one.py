355c53b737078a797a862f47d5398087d512e715
Godfred06 <95718771+Godfred06@users.noreply.github.com>
2022-01-06 19:29:47 +0000

    Update one.py

--- one.py
+++ one.py
@@ -3,6 +3,7 @@ lst= st.split()
 print (lst)
 punctuation_chars = ["'", '"', ",", ".", "!", ":", ";", '#', '@']
 def clear_punc(lists):
+   '''for every word in the list, the function checks to see if there is any unwanted characters at the end and removes it'''
     new_st=''
     new_list=[]
     for word in lists:
@@ -15,6 +16,7 @@ def clear_punc(lists):
 
 
 def longlenghts(strings):
+     '''checks the length of the string to see whether it is greater or equal to 4 and adds to a final list'''
     final_list =[string for string in strings if len(string)>=4]
     return final_list
 print (clear_punc(lst))
